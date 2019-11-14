2632dfb89e07c9349c31a780b484be601c8a8aa5
SaurabhKumbhar24 <45506009+SaurabhKumbhar24@users.noreply.github.com>
2019-11-14 20:20:11 +0530

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -1,16 +1,16 @@
-
+#Voltage vs Height of Fuel in Vehicles
 
 import numpy as np
 import matplotlib.pyplot as plt
 from pylab import*
 
-#plt.figure();
-#plt.plot(xvals,yvals)
-#plt.xlabel("Voltage")
-#plt.ylabel("Height")
-#plt.ylim([0,10])
-#plt.legend(loc='best')
-#np.polyfit()
+plt.figure();
+plt.plot(xvals,yvals)
+plt.xlabel("Voltage")
+plt.ylabel("Height")
+plt.ylim([0,10])
+plt.legend(loc='best')
+np.polyfit()
 ax = plt.subplot()
 scatter(xvals,yvals)
 plt.show()
