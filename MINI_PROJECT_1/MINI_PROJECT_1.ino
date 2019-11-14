29020e1c566afba68b469b6a59a377f3324035d9
SaurabhKumbhar24 <45506009+SaurabhKumbhar24@users.noreply.github.com>
2019-11-14 20:13:01 +0530

    Add files via upload

---
+++ MINI_PROJECT_1/MINI_PROJECT_1.ino
@@ -0,0 +1,18 @@
+const int voltage_sensor = A2;
+float voltage;
+float volt;
+int offset = 8;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+ //Voltage
+ volt = analogRead(voltage_sensor);
+ voltage = map(volt,0,1023,0,2500) + offset;
+ voltage /= 100;
+ Serial.print("Voltage :");
+ Serial.print(voltage);
+ Serial.println("V");
+ delay(1000);
+ }
---
+++ untitled1.py
@@ -0,0 +1,16 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import*
+
+#plt.figure();
+#plt.plot(xvals,yvals)
+#plt.xlabel("Voltage")
+#plt.ylabel("Height")
+#plt.ylim([0,10])
+#plt.legend(loc='best')
+#np.polyfit()
+ax = plt.subplot()
+scatter(xvals,yvals)
+plt.show()
